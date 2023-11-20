690d14f24404a271c50a8ec09c8243d8c8126559
Mehedi H Sajib <48852166+mehedihsajib@users.noreply.github.com>
2023-11-21 01:22:31 +0600

    package: keep react added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 "use client";
+import { AccordionComponent } from "@/components/Accordion";
+import { Button } from "keep-react";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello devlyrics</h1>
+      <Button size="md">Default</Button>
+      <AccordionComponent />
     </main>
   );
 }
